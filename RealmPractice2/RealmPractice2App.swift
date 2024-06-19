882c2333234efbda127a6ea175b74e550b2899c6
Natsukotochobi <makumaku2014@gmail.com>
2024-06-19 17:18:59 +0900

    first commit

---
+++ RealmPractice2/RealmPractice2App.swift
@@ -0,0 +1,17 @@
+//
+//  RealmPractice2App.swift
+//  RealmPractice2
+//
+//  Created by natsuko mizuno on 2024/06/19.
+//
+
+import SwiftUI
+
+@main
+struct RealmPractice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
